a9b411b02a2edfd0656df79df8403bed9bc7f27d
jonadepr <eljosele4@gmail.com>
2020-02-19 11:47:14 +0100

    Count en MainController y ProductoRepository

--- src/main/java/com/once/facturas/controller/MainController.java
+++ src/main/java/com/once/facturas/controller/MainController.java
@@ -1,22 +1,35 @@
 package com.once.facturas.controller;
 
+import com.once.facturas.model.ProductoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * MainController es el controlador de la aplicación Facturas,
- * que es una aplicación MVC (Modelo Vista Controlador)
+ * que es una aplicación tipo MVC (Modelo Vista Controlador)
  * 
  * 
  */
 
 @Controller
 class MainController {
-    @GetMapping("/hello") // Escucho al GET en /time
+    
+    @Autowired
+    ProductoRepository pr;
+
+    @GetMapping("/hello") // Escucho al GET en /hello
     @ResponseBody // Haré un body html para devolver la página completa
     public String hello() { // Método para devolver un string para responsebody
-        return "Hola Mundo"; // Devulevo Hola Mundo
+        return "Hola Mundo"; // Devuelvo Hola Mundo
     }
-    
+
+    @GetMapping("/count") // Escucho al GET en /count
+    @ResponseBody // Haré un body html para devolver la página completa
+    public String count() { // Método para devolver un string con responsebody
+        return "Tengo "+String.valueOf(pr.count())+" productos"; // Devuelvo el número de productos desde pr.count()
+    }
+
 }
