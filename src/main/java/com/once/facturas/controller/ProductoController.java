133b5f83de66946f0594c35092e16cc42b5eaedf
Paco <you@example.com>
2020-02-21 12:16:07 +0100

    Ejercicio put y delete

--- src/main/java/com/once/facturas/controller/ProductoController.java
+++ src/main/java/com/once/facturas/controller/ProductoController.java
@@ -1,5 +1,6 @@
 package com.once.facturas.controller;
 
+import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -10,9 +11,14 @@ import com.once.facturas.model.Producto;
 import com.once.facturas.model.ProductoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,40 +33,94 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping(value = "/productos")
+@RequestMapping(value = "/api/productos")
 class ProductoController {
 
     /**
      * Escucha en las siguientes rutas:
      * 
-     * GET /productos/ GET /productos/{id}/ GET /productos/hello GET
+     * GET /productos/ GET /productos/{id}/ GET /api/productos/hello GET
      * /productos/count
+     * 
      */
 
     @Autowired
     ProductoRepository pr;
+
+/**
+ * 
+ * listado elementos
+ * 
+ */
+
 
     @GetMapping("/")
     public Iterable<Producto> getAllProductos() {
         return pr.findAll();
     }
 
-    @GetMapping("{id}/")
+
+/**
+ * 
+ * Borrado elemento
+ * 
+ */
+   
+  @DeleteMapping("/{id}/")  // ("/{id}/{otroid}") 
+    public ResponseEntity borrarProducto(
+        @PathVariable("id") Long id) {
+      
+        try {  
+            
+            pr.deleteById(id);
+
+        }  catch  (EmptyResultDataAccessException e) {
+            
+            
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND); 
+
+        }   
+       
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+
+/**
+ * 
+ * buscar elementos
+ * 
+ */
+
+
+    @GetMapping("/{id}/")
     public Producto getProducto(@PathVariable("id") Long id) {
         System.out.println("valor devuelto: " + pr.findById(id));
-
+/*
         Optional<Producto> p = pr.findById(id);
 
         if (p.isEmpty())
             return null;
 
         return p.get();
-        /*
-         * try { return pr.findById(id).get(); } catch (NoSuchElementException e) {
-         * return null; }
-         */
+  
+  */
+        
+          try { return pr.findById(id).get(); 
+        } catch (NoSuchElementException e) {
+          return null; }
+         
 
     }
+
+/**
+ * 
+ * crear elementos
+ * 
+ */
+
+
+
 
     @PostMapping("/")
     public Producto crearProducto( 
@@ -71,11 +131,75 @@ class ProductoController {
         return p;
     }
 
+
+/**
+ * 
+ * modificar elemento
+ * 
+ */
+
+@PutMapping("/{id}/")
+public Producto modificarProducto(@RequestBody Producto producto,
+
+    @PathVariable("id") Long id) {
+
+        
+        Optional<Producto> p = pr.findById(id);
+
+        if (p.isEmpty())
+            return null;
+
+        else {
+             
+            
+            Producto pmod = pr.findById(id).get();
+        
+             pmod.setDescripcion(producto.getDescripcion());
+             pmod.setFabricante(producto.getFabricante());
+             pmod.setPrecio(producto.getPrecio());
+         
+              pmod = pr.save(producto);
+                 
+               return  pmod; 
+        }
+
+
+   
+   
+ /*  
+    Producto p = pr.findById(id).get();
+   
+    p.setDescripcion(producto.getDescripcion());
+    p.setFabricante(producto.getFabricante());
+    p.setPrecio(producto.getPrecio());
+
+     p = pr.save(producto);
+        
+      return  p;
+   */ 
+}
+
+
+
+
+
+/**
+ * Mapeo inicial de prueba - leer elementos
+ * 
+ */    
     @GetMapping("/hello") // Escucho al GET en /hello
     @ResponseBody // Haré un body html para devolver la página completa
     public String hello() { // Método para devolver un string para responsebody
         return "Hola Mundo"; // Devuelvo Hola Mundo
     }
+
+/**
+ * 
+ * contar elementos 
+ *
+ */
+
+
 
     @GetMapping("/count") // Escucho al GET en /count
     @ResponseBody // Haré un body html para devolver la página completa
