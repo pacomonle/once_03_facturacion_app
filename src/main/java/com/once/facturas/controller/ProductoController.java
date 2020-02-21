6baff34030b8a7463a7d87f87365d8db9296d1f8
Alfonso <afoone@hotmail.com>
2020-02-21 09:16:14 +0100

    id

--- src/main/java/com/once/facturas/controller/ProductoController.java
+++ src/main/java/com/once/facturas/controller/ProductoController.java
@@ -2,6 +2,7 @@ package com.once.facturas.controller;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import javax.websocket.server.PathParam;
 
@@ -11,14 +12,16 @@ import com.once.facturas.model.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 /**
- * MainController es el controlador de la aplicación Facturas,
- * que es una aplicación tipo MVC (Modelo Vista Controlador)
+ * MainController es el controlador de la aplicación Facturas, que es una
+ * aplicación tipo MVC (Modelo Vista Controlador)
  * 
  * 
  */
@@ -26,29 +29,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/productos")
 class ProductoController {
-    
+
+    /**
+     * Escucha en las siguientes rutas:
+     * 
+     * GET /productos/ GET /productos/{id}/ GET /productos/hello GET
+     * /productos/count
+     */
+
     @Autowired
     ProductoRepository pr;
 
-
     @GetMapping("/")
-    public Iterable<Producto> getAllProductos(){
+    public Iterable<Producto> getAllProductos() {
         return pr.findAll();
     }
 
     @GetMapping("{id}/")
-    public Producto getProducto(
-            @PathVariable("id") Long id
-        ){
-            
-         try {
-            return pr.findById(id).get(); 
-         } catch (NoSuchElementException e) {
-             return null;
-         }
-            
-        }
+    public Producto getProducto(@PathVariable("id") Long id) {
+        System.out.println("valor devuelto: " + pr.findById(id));
 
+        Optional<Producto> p = pr.findById(id);
+
+        if (p.isEmpty())
+            return null;
+
+        return p.get();
+        /*
+         * try { return pr.findById(id).get(); } catch (NoSuchElementException e) {
+         * return null; }
+         */
+
+    }
+
+    @PostMapping("/")
+    public Producto crearProducto( 
+        @RequestBody Producto producto
+     ) {
+         System.out.println(producto);
+        Producto p = pr.save(producto);
+        return p;
+    }
 
     @GetMapping("/hello") // Escucho al GET en /hello
     @ResponseBody // Haré un body html para devolver la página completa
@@ -59,7 +80,7 @@ class ProductoController {
     @GetMapping("/count") // Escucho al GET en /count
     @ResponseBody // Haré un body html para devolver la página completa
     public String count() { // Método para devolver un string con responsebody
-        return "Tengo "+String.valueOf(pr.count())+" productos"; // Devuelvo el número de productos desde pr.count()
+        return "Tengo " + String.valueOf(pr.count()) + " productos"; // Devuelvo el número de productos desde pr.count()
     }
 
 }
--- src/main/java/com/once/facturas/model/Producto.java
+++ src/main/java/com/once/facturas/model/Producto.java
@@ -19,7 +19,7 @@ public class Producto {
     @Size(min = 5, max = 300)
     private String descripcion;
 
-    @Size(min = 5, max = 300)
+    @Size(max = 300)
     private String fabricante;
 
     private Float precio;
@@ -46,6 +46,16 @@ public class Producto {
 
     public void setPrecio(Float precio) {
 		this.precio = precio;
-	}
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
     
 }
